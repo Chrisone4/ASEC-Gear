03eed6da16bbcabca7269d93f609a8d64632572a
Chrisone4 <64732994+Chrisone4@users.noreply.github.com>
2022-12-07 20:47:55 +1100

    Initial commit

---
+++ mod.cpp
@@ -0,0 +1,13 @@
+name = "ASEC Gear";
+dir = "@ASEC Gear";
+author = "Jup, Lip, Thunder";
+actionName = "";
+action = "";
+description = "Website: https://www.asecmilsim.com/";
+tooltip = "ASEC Gear";
+tooltipOwned = "ASEC Gear";
+overview = "ASEC Gear";
+overviewText = "ASEC Gear";
+logoSmall	 = "ASECGearSmall.paa";	// Display next to the item added by the mod
+logo		 = "ASECGear.paa";			// Logo displayed in the main menu
+logoOver	 = "ASECGear.paa";	
