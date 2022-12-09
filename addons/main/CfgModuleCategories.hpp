0055db5680e1b7b711431e60265cfe5d8274d6e4
Chrisone4 <64732994+Chrisone4@users.noreply.github.com>
2022-12-09 18:56:17 +1100

    Misc

--- addons/main/CfgModuleCategories.hpp
+++ addons/main/CfgModuleCategories.hpp
@@ -1,14 +1,8 @@
 class CfgFactionClasses {
     class NO_CATEGORY;
-    class PROJ_TEMPL: NO_CATEGORY {
-        displayName = "PROJ_TEMPL";
+    class ASEC_GEAR: NO_CATEGORY {
+        displayName = "ASEC_GEAR";
         priority = 2;
         side = 7;
-    };
-};
-
-class CfgVehicleClasses {
-    class PROJ_TEMPL_Vehicles {
-        displayName = "PROJ_TEMPL Vehicles";
     };
 };
--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ace_common"};
+        requiredAddons[] = {"cba_main"};
         author = "";
         authors[] = {"ACE Team", "Glowbal"};
         authorUrl = "http://ace3mod.com";
